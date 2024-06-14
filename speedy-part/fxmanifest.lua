6b6d585f4c6cdaff324c37fa7493023596def990
Ghgsnap <lihlsnap@yahoo.com>
2024-06-14 00:15:07 -0500

    Add files via upload

---
+++ speedy-part/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'bodacious'
+game 'gta5'
+version '1.0.0'
+description 'AUTO PARTS! STORE for MECHS'
+creator 'SNAP'
+
+data_file('DLC_ITYP_REQUEST')('stream/mbv_addons.ytyp')
+data_file('DLC_ITYP_REQUEST')('stream/mbv_vol1.ytyp')
+data_file('DLC_ITYP_REQUEST')('stream/mbv_vol2.ytyp')
+data_file('DLC_ITYP_REQUEST')('stream/mbv_vol3.ytyp')
+data_file('DLC_ITYP_REQUEST')('stream/mbv_vol4.ytyp')
